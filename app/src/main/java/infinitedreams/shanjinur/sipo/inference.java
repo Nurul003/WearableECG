86763c95e9cbceda6b296a6af384d8182d59e451
nurul003 <a.nurulhudahossain016.o@gmail.com>
2020-06-22 21:39:50 +0600

    inference lyaout added

---
+++ app/src/main/java/infinitedreams/shanjinur/sipo/inference.java
@@ -0,0 +1,13 @@
+package infinitedreams.shanjinur.sipo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class inference extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inference);
+    }
+}
